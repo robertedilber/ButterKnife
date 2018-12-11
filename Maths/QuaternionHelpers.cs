d39bd478ee7cb140d397eda46bd597e810cb80b1
Robert <robertedilber@gmail.com>
2018-12-11 16:42:27 -0500

    Oh wow

--- Maths/QuaternionHelpers.cs
+++ Maths/QuaternionHelpers.cs
@@ -2,9 +2,15 @@
 
 public static class QuaternionHelpers
 {
-	public static Quaternion GetDelta(this Quaternion quat, Quaternion target)
-		=> target * Quaternion.Inverse(quat);
+    public static Quaternion GetDelta(this Quaternion quat, Quaternion target)
+        => target * Quaternion.Inverse(quat);
 
-	public static Quaternion ApplyDelta(this Quaternion quat, Quaternion delta)
-		=> quat * delta;
+    public static Quaternion ApplyDelta(this Quaternion quat, Quaternion delta)
+        => quat * delta;
+
+    public static Quaternion Define(Vector3 axis, float theta)
+    {
+        Vector3 ijk = Mathf.Sin(theta / 2) * axis;
+        return new Quaternion(ijk.x, ijk.y, ijk.z, Mathf.Cos(theta / 2));
+    }
 }
