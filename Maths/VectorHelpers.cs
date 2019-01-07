e7a8c50d7277a0584be02743fc6a884e94da2334
Robert <robertedilber@gmail.com>
2019-01-07 10:51:59 -0500

    CHOSE

--- Maths/VectorHelpers.cs
+++ Maths/VectorHelpers.cs
@@ -1,22 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public static class VectorHelpers
 {
     #region Helpers
 
     public static Vector3 ProjectOnPlane(Vector3 vectorToProject, Vector3 normal)
-        => vectorToProject - Dot(normal, vectorToProject) * normal;
+        => vectorToProject - ((normal.x * vectorToProject.x) + (normal.y * vectorToProject.y) + (normal.z * vectorToProject.z)) * normal;
 
     public static Vector3 ProjectOnPlane(Vector3 vectorToProject, Vector3 normal, Vector3 planeOrigin)
-        => vectorToProject - Dot(normal, (vectorToProject - planeOrigin)) * normal;
+    {
+        Vector3 v = vectorToProject - planeOrigin;
+        return vectorToProject - ((normal.x * v.x) + (normal.y * v.y) + (normal.z * v.z)) * normal;
+    }
 
-    public static float Dot(Vector3 a, Vector3 b)
-        => (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+    public static float Dot(Vector3 a, Vector3 b) => (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 
-    public static Vector3 Cross(Vector3 a, Vector3 b)
-        => new Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+    public static Vector3 Cross(Vector3 a, Vector3 b) => new Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
 
-    public static Vector3 GetCentroid(Vector3[] points)
+    public static Vector3 Centroid(Vector3[] points)
     {
         float[] centroid = new float[] { 0.0f, 0.0f, 0.0f };
         int numberOfPoints = points.Length;
@@ -33,16 +35,16 @@ public static class VectorHelpers
         return new Vector3(centroid[0], centroid[1], centroid[2]);
     }
 
-    public static Vector3 GetMiddlePoint(Vector3 a, Vector3 b) => a + (b - a) / 2;
+    public static Vector3 Centroid(Vector3 a, Vector3 b) => a + (b - a) / 2;
 
-    public static float GetAngeInRadians(Vector3 a, Vector3 b)
+    public static float AngleInRadians(Vector3 a, Vector3 b, Vector3 normal)
     {
         a.Normalize();
         b.Normalize();
         float[] cross = new float[] { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x };
-        float[] normal = new float[] { Mathf.Abs(cross[0]), Mathf.Abs(cross[1]), Mathf.Abs(cross[2]) };
-        float orientation = normal[0] * cross[0] + normal[1] * cross[1] + normal[2] * cross[2];
-        float angle = Mathf.Acos(a.x * b.x + a.y * b.y + a.z * b.z) * Mathf.Sign(orientation);
+        float orientation = normal.x * cross[0] + normal.y * cross[1] + normal.z * cross[2];
+        orientation = orientation < 0.0f ? -1 : 1;
+        float angle = Mathf.Acos(Mathf.Clamp(a.x * b.x + a.y * b.y + a.z * b.z, -1, 1)) * Mathf.Sign(orientation);
         return angle + (angle < 0.0f ? 2 * Mathf.PI : 0);
     }
 
@@ -51,6 +53,12 @@ public static class VectorHelpers
     #region Extensions Methods
 
     public static Vector3 Absolute(this Vector3 a) => new Vector3(Mathf.Abs(a.x), Mathf.Abs(a.y), Mathf.Abs(a.z));
+    public static void Absolute(ref this Vector3 a)
+    {
+        a.x = Mathf.Abs(a.x);
+        a.y = Mathf.Abs(a.y);
+        a.z = Mathf.Abs(a.z);
+    }
 
     #endregion
 }
